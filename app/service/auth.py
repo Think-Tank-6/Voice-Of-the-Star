ba26785988c10ca7ff6af9e9e5d716fc7206bcbe
neymaru <106804514+neymaru@users.noreply.github.com>
2023-12-29 12:14:11 +0900

    [feat #9] 로그인 API, jwt 생성 service 구현

--- app/service/auth.py
+++ app/service/auth.py
@@ -1,11 +1,15 @@
 import bcrypt
 from datetime import datetime, timedelta
 from jose import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class AuthService:
     encoding: str = "UTF-8"
-    secret_key: str = "be2aed6fab97adc5116c8e844354b820530803d144f2c89858da20072ad74c80"
-    jwt_algorithm = "HS256" # 웹토큰 생성에 사용되는 알고리즘
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM") # 웹토큰 생성에 사용되는 알고리즘
 
     def hash_password(self, plain_password: str) -> str:
         hashed_password: bytes = bcrypt.hashpw(
@@ -26,6 +30,6 @@ class AuthService:
                 "sub": user_id,
                 "exp": datetime.now() + timedelta(days=1),  # 토큰의 만료시간 = 하루(요청한 시간부터)
             }, 
-            self.secret_key, 
-            algorithm=self.jwt_algorithm
+            self.SECRET_KEY, 
+            algorithm=self.JWT_ALGORITHM
         )
