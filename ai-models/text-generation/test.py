a80709eb4682b431ee2f6eed422b1816bc5cdabd
KimYeongHun <qkfkdka@naver.com>
2024-01-04 21:08:34 +0900

    [feat #29] Prompt 입력 텍스트 전처리 구현

--- ai-models/text-generation/test.py
+++ ai-models/text-generation/test.py
@@ -1,17 +1,11 @@
-from preprocessing import get_user_name,save_messages,extract_messages
 import os
-
-input_directory = 'input/'
-output_directory = 'output/'
+from preprocessing import get_user_name, extract_messages
 
 if __name__ == '__main__':
-    for filename in os.listdir(input_directory):
-        if filename.endswith('.txt'):
-            full_input_path = os.path.join(input_directory, filename)
-            user_name = get_user_name(full_input_path)
 
-            if user_name:
-                user_messages = extract_messages(full_input_path, user_name)
-                if user_messages:
-                    full_output_path = os.path.join(output_directory, f'extracted_messages_{filename}')
-                    save_messages(user_messages, full_output_path)
+    file_content = ""
+
+    user_name = get_user_name(file_content)
+
+    if user_name:
+        user_messages = extract_messages(file_content, user_name)
