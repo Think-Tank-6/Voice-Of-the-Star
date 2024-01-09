a0a09cbea478f70761bb9e043235ba0dc9b009d6
KimYeongHun <qkfkdka@naver.com>
2024-01-09 18:59:30 +0900

    [feat #87] gpt 모듈화

--- backend/app/api/star.py
+++ backend/app/api/star.py
@@ -8,7 +8,7 @@ from database.orm import Star, User
 from database.repository import UserRepository, StarRepository, ChatRepository
 from schema.request import CreateStarRequest
 from schema.response import StarListSchema, StarSchema
-from service.ai_serving import TextGeneration
+from service.ai_serving import PromptGeneration
 from service.ai_serving import SpeakerIdentification
 
 router = APIRouter(prefix="/stars")
@@ -87,8 +87,8 @@ async def create_star_handler(
     }
 
     # Text Generation
-    # text_generator = TextGeneration(request=request)
-    # chat_prompt_input_data = text_generator.create_prompt_input()
+    # prompt_generator = PromptGeneration(request=request)
+    # chat_prompt_input_data = prompt_generator.create_prompt_input()
 
     chat_prompt_input_data = "test"
 
@@ -155,4 +155,161 @@ def delete_star_handler(
     if not star:
         raise HTTPException(status_code=404, detail="Star Not Found")
     star_repo.delete_star(star_id=star_id)
+
+# from datetime import date
+# from typing import List, Optional
+# from fastapi import Depends, File, Form, HTTPException, APIRouter, UploadFile
+# from service.auth import AuthService
+# from security import get_access_token
+
+# from database.orm import Star, User
+# from database.repository import UserRepository, StarRepository, ChatRepository
+# from schema.request import CreateStarRequest
+# from schema.response import StarListSchema, StarSchema
+# from service.ai_serving import PromptGeneration
+
+# router = APIRouter(prefix="/stars")
+
+
+# # 유저 검증 및 조회(공통)
+# def get_authenticated_user(
+#     access_token: str = Depends(get_access_token),
+#     auth_service: AuthService = Depends(),
+#     user_repo: UserRepository = Depends(),
+# ) -> User:
+#     return auth_service.verify_user(access_token=access_token, user_repo=user_repo)
+
+
+# # 전체 star 조회
+# @router.get("", status_code=200)
+# def get_stars_handler(
+#     order: str | None = None,
+#     user: User = Depends(get_authenticated_user),   # 유저 검증 dependency
+# ) -> StarListSchema:
+    
+#     stars: List[Star] = user.stars
+
+#     if order and order == "DESC":
+#         return StarListSchema(
+#             stars=[StarSchema.from_orm(star) for star in stars[::-1]]  
+#         )
+#     return StarListSchema(
+#         stars=[StarSchema.from_orm(star) for star in stars] 
+#     )
+
+# @router.get("/{star_id}/last", status_code=200)
+# def get_last_message(
+#     star_id: str, 
+#     chat_repo: ChatRepository = Depends()
+# ):
+#     last_message = chat_repo.get_last_message(star_id)
+#     return last_message
+
+# # 단일 star 조회
+# @router.get("/{star_id}", status_code=200)
+# def get_star_handler(
+#     star_id: int,
+#     user: User = Depends(get_authenticated_user),   # 유저 검증 dependency
+#     star_repo: StarRepository = Depends(),
+# ) -> StarSchema:
+    
+#     star: Star | None = star_repo.get_star_by_star_id(star_id=star_id, user_id=user.user_id)
+
+#     if star:
+#         return StarSchema.from_orm(star)
+#     raise HTTPException(status_code=404, detail="Star Not Found")
+
+
+# # star 생성
+# @router.post("", status_code=201)
+# async def create_star_handler(
+#     star_name: str = Form(...),
+#     gender: str = Form(...),
+#     birth: date = Form(...),
+#     death_date: date = Form(...),
+#     relationship: str = Form(...),
+#     persona: Optional[str] = Form(...),
+#     original_text_file: UploadFile = File(...),
+#     user: User = Depends(get_authenticated_user),   # 유저 검증 dependency
+#     star_repo: StarRepository = Depends(StarRepository),
+# ) -> StarSchema:
+    
+#     request = {
+#         "star_name": star_name,
+#         "gender": gender,
+#         "birth": birth,
+#         "death_date": death_date,
+#         "relationship": relationship,
+#         "persona": persona,
+#     }
+
+#     # Text Generation
+#     # prompt_generator = PromptGeneration(request=request)
+#     # chat_prompt_input_data = prompt_generator.create_prompt_input()
+
+#     chat_prompt_input_data = "test"
+
+#     # DB Save
+#     star: Star = Star.create(
+#         request=request, 
+#         chat_prompt_input_data=chat_prompt_input_data,
+#         user_id=user.user_id
+#     )  
+#     star: Star = star_repo.create_star(star=star)
+
+#     return StarSchema.from_orm(star)
+
+
+# # star 생성(보이스 업로드)
+# @router.post("/voice-upload", status_code=200)
+# def upload_voice_handler(
+#     original_voice_file: UploadFile = File(...),
+#     user: User = Depends(get_authenticated_user)
+#     ):
+
+#     speaker_identification = SpeakerIdentification(original_voice_file)
+#     speaker_num, speaker_sample_list = speaker_identification.get_speaker_samples(original_voice_file)
+    
+
+#     return {"speaker_num":speaker_num, "speaker_sample_list":speaker_sample_list}
+
+# # star 생성(보이스 선택)
+# @router.post("/voice-select", status_code=201)
+# def upload_voice_handler(
+#     user: User = Depends(get_authenticated_user),  
+# ) -> StarSchema:
+#     return {"message":"voice select 페이지"}
+
+
+# # star 수정
+# @router.patch("/{star_id}", status_code=200)
+# def update_star_handler(
+#     star_id: int, 
+#     request: CreateStarRequest,
+#     user: User = Depends(get_authenticated_user),   # 유저 검증 dependency
+#     star_repo: StarRepository = Depends(StarRepository),
+# ) -> StarSchema:
+    
+#     star: Star | None = star_repo.get_star_by_star_id(star_id=star_id, user_id=user.user_id)
+
+#     if star:
+#         star: Star = star.update(request=request)
+#         star: Star = star_repo.update_star(star=star)
+#         return StarSchema.from_orm(star)
+#     return HTTPException(status_code=404, detail="Star Not Found")
+
+
+# # star 삭제
+# @router.delete("/{star_id}", status_code=204)
+# def delete_star_handler(
+#     star_id: int,
+#     user: User = Depends(get_authenticated_user),   # 유저 검증 dependency
+#     star_repo: StarRepository = Depends(StarRepository),
+# ):
+    
+#     star: Star | None = star_repo.get_star_by_star_id(star_id=star_id, user_id=user.user_id)
+    
+#     if not star:
+#         raise HTTPException(status_code=404, detail="Star Not Found")
+#     star_repo.delete_star(star_id=star_id)
 
